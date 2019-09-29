bfbc160702fd21b5ecd707b2aaa4573518c04392
Brandon McClure <brandonmcclure89@gmail.com>
2019-09-29 17:39:30 -0600

    Remove WinForm Sprite and Mode classes which are not being used with the new wpf approach

--- UnitTests/AMode_Test.cs
+++ UnitTests/AMode_Test.cs
@@ -6,28 +6,5 @@ using MyScreensaver_wpf;
 
 namespace UnitTests
 {
-    [TestClass]
-    public class AMode_Test
-    {
-        [TestMethod]
-        public void moveTimer_Tick_ShouldCallTheTickFunctionOnAllSprite()
-        {
-            AMode aMode = new AMode();
-            List<Mock<ASprite>> mockedSprites = new List<Mock<ASprite>>();
 
-            for (int index = 0; index <= 9; index++){
-                Mock<ASprite> sprite = new Mock<ASprite>();
-                aMode.Sprite_wfCollection.Add(sprite.Object);
-                mockedSprites.Add(sprite);
-            }
-           
-
-            aMode.moveTimer_Tick(new object(),new EventArgs());
-
-            for (int index = 0; index <= 9; index++)
-            {
-                mockedSprites[index].Verify(mock => mock.tick(), Times.Once());
-            }
-        }
-    }
 }
